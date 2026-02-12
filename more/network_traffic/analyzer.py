93bbd45ca6b08bb326292ffad4a0fe712bdfd5b8
elihalperin2003 <eelihalperin@gmail.com>
2026-02-12 14:46:57 +0200

    analyze_night_activity

--- more/network_traffic/analyzer.py
+++ more/network_traffic/analyzer.py
@@ -1,15 +1,17 @@
-from checks import check_ip_external_addresses, check_port_sensitive, check_large_packet
+from checks import check_ip_external_addresses, check_port_sensitive, check_large_packet, check_night_activity
 
 
 def analyze_ip_external_addresses(file):
     return [line[1] for line in file if check_ip_external_addresses(line)]
-# ddd
 
 def analyze_port_sensitive(file):
     return [line for line in file if check_port_sensitive(line)]
 
 def analyze_large_packet(file):
     return [line for line in file if check_large_packet(line)]
+
+def analyze_night_activity(file):
+    return [line for line in file if check_night_activity(line)]
 
 def labeling_size_packet(file):
     return [line.append("LARGE") if check_large_packet(line) else line.append("NORMAL") for line in file]
--- more/network_traffic/checks.py
+++ more/network_traffic/checks.py
@@ -1,4 +1,5 @@
-from config import EXTERNAL_IP, SENSITIVE_PORT, LARGE_PACKET
+from config import EXTERNAL_IP, SENSITIVE_PORT, LARGE_PACKET, NIGHT_ACTIVITY
+from datetime import datetime
 
 def check_ip_external_addresses(line):
     if not any(line[1].startswith(_) for _ in EXTERNAL_IP):
@@ -14,3 +15,16 @@ def check_large_packet(line):
     if line[5] > LARGE_PACKET:
         return True
     return False
+
+def check_night_activity(line):
+    def sum_minute(time):
+        return (time.hour * 60) + time.minute
+    hour_line = datetime.strptime(line[0],"%Y-%m-%d %H:%M:%S")
+    start = datetime.strptime(NIGHT_ACTIVITY["start"],"%H:%M")
+    end = datetime.strptime(NIGHT_ACTIVITY["end"], "%H:%M")
+    if sum_minute(start) < sum_minute(hour_line) < sum_minute(end):
+        return True
+    return False
+
+c = ["2024-01-15 04:01:00","192.168.1.100","10.0.0.5","443","HTTPS","1024"]
+print(check_night_activity(c))
