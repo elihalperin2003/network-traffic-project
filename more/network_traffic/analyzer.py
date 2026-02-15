c8ec5f53e2c2f64598bab56172f1527ee4be1743
elihalperin2003 <eelihalperin@gmail.com>
2026-02-15 11:43:23 +0200

    suspicious_detection_for_ips #2

--- more/network_traffic/analyzer.py
+++ more/network_traffic/analyzer.py
@@ -1,4 +1,5 @@
 from checks import check_ip_external_addresses, check_port_sensitive, check_large_packet, check_night_activity
+from collections import defaultdict
 
 
 def analyze_ip_external_addresses(file):
@@ -31,18 +32,21 @@ def port_to_protocol_mapping(file):
 
 
 def suspicious_detection_for_ips(file):
-    checks = [["EXTERNAL_IP", check_ip_external_addresses], ["SENSITIVE_PORT", check_port_sensitive],
-              ["LARGE_PACKET", check_large_packet], ["NIGHT_ACTIVITY", check_night_activity]]
-    return {line[1]: {chack[0] for chack in checks if chack[1](line)} for line in file}
+    checks = [["EXTERNAL_IP", check_ip_external_addresses],["SENSITIVE_PORT", check_port_sensitive],["LARGE_PACKET", check_large_packet],["NIGHT_ACTIVITY", check_night_activity]]
+    suspicion_report = defaultdict(set)
+    for line in file:
+        for suspicion_name, check_func in checks:
+            if check_func(line):
+                suspicion_report[line[1]].add(suspicion_name)
+    return {ip: list(suspicions) for ip, suspicions in suspicion_report.items() if suspicions}
 
-
-d = [["2024-01-15 08:00:29", "10.1.0.8", "10.0.0.7", "54", "HTTP", "762"],
-     ["2024-01-15 03:00:29", "10.1.0.8", "10.0.0.7", "80", "HTTP", "762"],
-     ["2024-01-15 03:00:29", "10.1.0.8", "10.0.0.7", "80", "HTTP", "50000"],
-     ["2024-01-15 03:00:29", "10.1.0.8", "10.0.0.7", "80", "HTTP", "762"],
-     ["2024-01-15 03:00:29", "10.4.0.8", "10.0.0.7", "80", "HTTP", "10000"]
-     ]
-
-dd = [["2024-01-15 08:00:29", "10.1.0.8", "10.0.0.7", "23", "HTTP", "762"],
-      ["2024-01-15 08:00:29", "10.1.0.8", "10.0.0.7", "54", "HTTP", "76200"]]
-print(suspicious_detection_for_ips(dd))
+# d = [["2024-01-15 08:00:29", "10.1.0.8", "10.0.0.7", "54", "HTTP", "762"],
+#      ["2024-01-15 03:00:29", "10.1.0.8", "10.0.0.7", "80", "HTTP", "762"],
+#      ["2024-01-15 03:00:29", "10.2.0.8", "10.0.0.7", "80", "HTTP", "50000"],
+#      ["2024-01-15 03:00:29", "10.1.0.8", "10.0.0.7", "80", "HTTP", "762"],
+#      ["2024-01-15 03:00:29", "10.4.0.8", "10.0.0.7", "80", "HTTP", "10000"]
+#      ]
+#
+# dd = [["2024-01-15 04:00:29", "10.1.0.8", "10.0.0.7", "23", "HTTP", "762"],
+#       ["2024-01-15 08:00:29", "10.1.0.8", "10.0.0.7", "54", "HTTP", "76200"]]
+# print(analyze_network_data(d))
